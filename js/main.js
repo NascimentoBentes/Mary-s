5f43e256c4382f0482fbadc33d6667830bc69d79
NascimentoBentes <alicebentes74@gmail.com>
2025-01-13 13:15:24 -0400

    Add Carrinho

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
-const showMenu = (toggleId,navId) =>{
-  const toggle = document.getElementById(toggleId), 
-          nav = document.getElementById(navId)
+/*=============== SHOW MENU ===============*/
+const showMenu = (toggleId, navId) =>{
+  const toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navId)
 
-  toggle.addEventListener('click',()=>{
-    nav.classList.toggle('show-menu')
-    toggle.classList.toggle('show-icon')
+  toggle.addEventListener('click', () =>{
+      // Add show-menu class to nav menu
+      nav.classList.toggle('show-menu')
+
+      // Add show-icon to show and hide the menu icon
+      toggle.classList.toggle('show-icon')
   })
 }
+
 showMenu('nav-toggle','nav-menu')
